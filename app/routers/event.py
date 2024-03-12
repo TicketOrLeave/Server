343da8c69f5c9c1d54352da868e5741d0cdab0bd
EmadAnwer <emadanwer.official@gmail.com>
2024-03-13 01:47:58 +0200

    [add] events TODO

---
+++ app/routers/event.py
@@ -0,0 +1,12 @@
+# TODO
+"""
+    /organizer/{organizer_id}/event
+    - Create a new event 
+    - Get all events 
+    - Get a single event 
+    - Update an event
+    - Delete an event
+
+
+
+"""
