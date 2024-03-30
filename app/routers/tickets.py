0e3a4c05b4afcb0b8065dd39f1f8cef6ef161a01
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-30 19:43:53 +0200

    [add] delete ticket

--- app/routers/tickets.py
+++ app/routers/tickets.py
@@ -1,7 +1,7 @@
 import select
 from typing import List
 from uuid import UUID
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Response
 from app.models import (
     Event,
     UserOrganizationRole,
@@ -38,3 +38,31 @@ async def get_tickets(
         raise HTTPException(status_code=401, detail="User is not the owner of the organization")
     tickets: List[Ticket] = db.exec(select(Ticket).where(Ticket.event_id == event_id)).all()
     return tickets
+
+
+@router.delete('/{ticket_id}', tags=["tickets"])
+async def delete_ticket(
+        request: Request,
+        ticket_id: UUID,
+        db: Session = Depends(get_db_session)
+):
+    user = request.state.user
+    ticket: Ticket = db.exec(select(Ticket).where(Ticket.id == ticket_id)).first()
+    if not ticket:
+        raise HTTPException(status_code=404, detail="Ticket not found")
+    event: Event = db.exec(select(Event).where(Event.id == ticket.event_id)).first()
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+    user_org_role: UserOrganizationRole = db.exec(
+        select(UserOrganizationRole).where(
+            UserOrganizationRole.user_id == user.id,
+            UserOrganizationRole.organization_id == event.organization_id,
+        )
+    ).first()
+    if not user_org_role:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    if user_org_role.user_role not in [UserRole.creator, UserRole.admin]:
+        raise HTTPException(status_code=401, detail="User is not the owner of the organization")
+    db.delete(ticket)
+    db.commit()
+    return Response(status_code=204)
